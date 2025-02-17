1df65651138b44aaba80236f0854b948001fd541
naourk <sofiamikaelafrancisco@gmail.com>
2025-02-17 19:13:57 +0400

    fixed the algorithm so that 2 point approach returns biggest circle if no valid circle is found & fixed 3 point approach calculations

--- algorithm.py
+++ algorithm.py
@@ -9,78 +9,39 @@ class Circle:
 
 def readDataset(file):
     with open(file, 'r') as f:
-        first_line = f.readline()  # Read the first line
-        lines = f.readlines()  # Read the remaining lines
+        first_line = f.readline() 
+        lines = f.readlines()  
     points = []
     for line in lines:
         x, y = line.split()
         points.append((float(x), float(y)))
-    return points, int(first_line)  # Return the points and the number of points
-
-def determinant(matrix):
-    return (matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1]) -
-            matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0]) +
-            matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]))
-
-def threePointApproach(points, n):
-    minCircle = Circle()
-    
-    for i in range(0, n-2):
-        for j in range(i+1, n-1):
-            for k in range(j+1, n):
-                # Matrix setup
-                mtx_a = [
-                    [points[i][0], points[i][1], 1],
-                    [points[j][0], points[j][1], 1],
-                    [points[k][0], points[k][1], 1]
-                ]
-                mtx_d = [
-                    [points[i][0]**2 + points[i][1]**2, points[i][1], 1],
-                    [points[j][0]**2 + points[j][1]**2, points[j][1], 1],
-                    [points[k][0]**2 + points[k][1]**2, points[k][1], 1]
-                ]
-                mtx_e = [
-                    [points[i][0]**2 + points[i][1]**2, points[i][0], 1],
-                    [points[j][0]**2 + points[j][1]**2, points[j][0], 1],
-                    [points[k][0]**2 + points[k][1]**2, points[k][0], 1]
-                ]
-                mtx_f = [
-                    [points[i][0]**2 + points[i][1]**2, points[i][0], points[i][1]],
-                    [points[j][0]**2 + points[j][1]**2, points[j][0], points[j][1]],
-                    [points[k][0]**2 + points[k][1]**2, points[k][0], points[k][1]]
-                ]
-
-                a = determinant(mtx_a)
-
-                if a == 0:
-                    continue
-
-                d = determinant(mtx_d)
-                e = determinant(mtx_e)
-                f = determinant(mtx_f)
-
-                xc = d / (2 * a)
-                yc = -e / (2 * a)
-                rc2 = (d**2 + e**2 - 4 * a * f) / (4 * a**2)
-
-                valid = True
-                for l in range(0, n):
-                    if ((points[l][0] - xc)**2 + (points[l][1] - yc)**2) > rc2:
-                        valid = False
-                        break
-                
-                if valid:
-                    if rc2 < minCircle.r:
-                        minCircle.x = xc
-                        minCircle.y = yc
-                        minCircle.r = m.sqrt(rc2)
-                        minCircle.area = m.pi * rc2
-
-    return minCircle.x, minCircle.y, minCircle.r, minCircle.area
+    return points, int(first_line)  
 
 def twoPointApproach(points, n):
     minCircle = Circle()
+    minCirclePoints = []
     
+    # find largest distance between 2 points
+    maxDist = 0
+    for i in range(0, n-1):
+        for j in range(i+1, n):
+            dist = m.sqrt((points[i][0] - points[j][0])**2 + (points[i][1] - points[j][1])**2)
+            if dist > maxDist:
+                maxDist = dist
+                minCirclePoints = [points[i], points[j]]
+    
+    # set largest circle as minCircle if there are no valid circles
+    if minCirclePoints:
+        xc = (minCirclePoints[0][0] + minCirclePoints[1][0]) / 2
+        yc = (minCirclePoints[0][1] + minCirclePoints[1][1]) / 2
+        rc2 = (maxDist / 2)**2
+        minCircle.x = xc
+        minCircle.y = yc
+        minCircle.r = m.sqrt(rc2)
+        minCircle.area = m.pi * rc2
+
+    found = True
+
     for i in range(0, n-1):
         for j in range(i+1, n):
             xc = (points[i][0] + points[j][0]) / 2
@@ -94,10 +55,54 @@ def twoPointApproach(points, n):
                     break
             
             if valid:
-                if m.sqrt(rc2) < minCircle.r:
+                found = True
+                if rc2 < minCircle.r:
                     minCircle.x = xc
                     minCircle.y = yc
                     minCircle.r = m.sqrt(rc2)
                     minCircle.area = m.pi * rc2
+                    minCirclePoints = [points[i], points[j]]
     
+    found = "" if found else "No valid circles found, returned circle with largest distance between 2 points."
+    print(found)
+    print("2-point approach:", minCirclePoints)
+    print("Midpoint:", minCircle.x, minCircle.y, "\nRadius:", minCircle.r, "\nArea:", minCircle.area)
+    return minCircle.x, minCircle.y, minCircle.r, minCircle.area
+
+def threePointApproach(points, n):
+    minCircle = Circle()
+    minCirclePoints = []
+    
+    for i in range(0, n-2):
+        for j in range(i+1, n-1):
+            for k in range(j+1, n):
+                A = points[i]
+                B = points[j]
+                C = points[k]
+
+                D = 2 * (A[0] * (B[1] - C[1]) + B[0] * (C[1] - A[1]) + C[0] * (A[1] - B[1]))
+
+                if D == 0:
+                    continue
+
+                xc = ((A[0]**2 + A[1]**2) * (B[1] - C[1]) + (B[0]**2 + B[1]**2) * (C[1] - A[1]) + (C[0]**2 + C[1]**2) * (A[1] - B[1])) / D
+                yc = ((A[0]**2 + A[1]**2) * (C[0] - B[0]) + (B[0]**2 + B[1]**2) * (A[0] - C[0]) + (C[0]**2 + C[1]**2) * (B[0] - A[0])) / D
+                rc2 = (A[0] - xc)**2 + (A[1] - yc)**2
+
+                valid = True
+                for l in range(0, n):
+                    if ((points[l][0] - xc)**2 + (points[l][1] - yc)**2) > rc2:
+                        valid = False
+                        break
+                
+                if valid:
+                    if rc2 < minCircle.r:
+                        minCircle.x = xc
+                        minCircle.y = yc
+                        minCircle.r = m.sqrt(rc2)
+                        minCircle.area = m.pi * rc2
+                        minCirclePoints = [points[i], points[j], points[k]]
+
+    print("3-point approach:", minCirclePoints)
+    print("Midpoint:", minCircle.x, minCircle.y, "\nRadius:", minCircle.r, "\nArea:", minCircle.area)
     return minCircle.x, minCircle.y, minCircle.r, minCircle.area
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from algorithm import readDataset, twoPointApproach, threePointApproach
+import matplotlib.pyplot as plt
 
-points, n = readDataset('datasets/circle_10.txt')
-
-print("2 point approach: ",twoPointApproach(points, n)) # test
-print("3 point approach: ",threePointApproach(points, n)) # test
-
-# make a for loop to read dataset and run twoPointApproach and threePointApproach for each dataset
-
+for file in ['datasets/circle_10.txt', 'datasets/circle_20.txt', 'datasets/circle_40.txt', 
+             'datasets/circle_100.txt', 'datasets/circle_400.txt']:
+    
+    points, n = readDataset(file)
+    print(f"\n{file}:")
+    x2, y2, r2, area2 = twoPointApproach(points, n)
+    print()
+    x3, y3, r3, area3 = threePointApproach(points, n)
+    print()
